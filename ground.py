380f3819ada10ad4929d0436434e1e0e79b7cfed
Ibrian Mihai-Razvan <ibrianmihairazvan@yahoo.com>
2025-01-27 13:48:12 +0200

    clean code ground

--- ground.py
+++ ground.py
@@ -3,15 +3,13 @@ import pygame
 class Ground(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, image, ground_height, move_type=None, move_range=0, move_speed=0):
         super().__init__()
-        # Scale the image to the platform's dimensions
+
         self.image = pygame.transform.scale(image, (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        # Define collision rect
         self.ground_rect = pygame.Rect(x, y + height - ground_height, width, ground_height)
 
-        # Movement attributes
         self.move_type = move_type  # None, "horizontal", or "vertical"
         self.move_range = move_range
         self.move_speed = move_speed
@@ -19,7 +17,6 @@ class Ground(pygame.sprite.Sprite):
         self.direction = 1  # 1 for forward, -1 for backward
 
     def update(self):
-        # Update the platform's main rectangle based on movement type
         if self.move_type == "horizontal":
             self.rect.x += self.move_speed * self.direction
         if abs(self.rect.x - self.start_pos[0]) > self.move_range:
@@ -29,7 +26,6 @@ class Ground(pygame.sprite.Sprite):
         if abs(self.rect.y - self.start_pos[1]) > self.move_range:
             self.direction *= -1
 
-        # Update the ground rectangle to match the platform's new position
         self.ground_rect.x = self.rect.x
         self.ground_rect.y = self.rect.y + self.rect.height - self.ground_rect.height
 

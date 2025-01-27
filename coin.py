58dde2f4203b663e8d9f4c799e0c919d29d759fd
Ibrian Mihai-Razvan <ibrianmihairazvan@yahoo.com>
2025-01-27 13:50:56 +0200

    clean code coin

--- coin.py
+++ coin.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 class Coin(pygame.sprite.Sprite):
     def __init__(self, frames, x, y):
         super().__init__()
@@ -18,30 +17,25 @@ class Coin(pygame.sprite.Sprite):
 
     def update(self):
         now = pygame.time.get_ticks()
-        if now - self.last_update > self.animation_speed:  # Change frame every 100 ms
-            # Loop through frames
+        if now - self.last_update > self.animation_speed:
             self.current_frame = (self.current_frame + 1) % len(self.frames)
             self.image = self.frames[self.current_frame]
             self.image = pygame.transform.scale(self.image, (50, 50))
             self.last_update = now
 
-
 def display_score(screen, SCREEN_WIDTH, score):
-    font = pygame.font.Font(None, 36)  # Use default font and size
+    font = pygame.font.Font(None, 36)
     text = f'Score: {score}'
 
-    # Render the main text
     main_text = font.render(text, True, (255, 255, 255))
     
-    # Render the stroke (black text) with offsets
-    stroke_color = (0, 0, 0)  # Black
-    offsets = [(-1, -1), (-1, 1), (1, -1), (1, 1)]  # Top-left, top-right, bottom-left, bottom-right
+    stroke_color = (0, 0, 0)
+    offsets = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
     for offset in offsets:
         stroke_text = font.render(text, True, stroke_color)
         screen.blit(stroke_text, (SCREEN_WIDTH - 150 + offset[0], 10 + offset[1]))
     
-    # Draw the main text on top
-    screen.blit(main_text, (SCREEN_WIDTH - 150, 10))  # Position at top right
+    screen.blit(main_text, (SCREEN_WIDTH - 150, 10))
 
 def display_winner(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
     font = pygame.font.Font("assets/win-font.ttf", 72)
